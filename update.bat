f508a95d6f7f1d3fd55bb43860ef4793eba65461
Denisbek1 <abd.den05@gmail.com>
2026-03-12 05:48:36 +0300

    site update

--- update.bat
+++ update.bat
@@ -1,6 +1,16 @@
 @echo off
+cd /d %~dp0
+
+echo Checking status...
 git status
+
+echo Adding files...
 git add .
-git commit -m "update"
+
+echo Commit...
+git commit -m "site update"
+
+echo Push to GitHub...
 git push origin main
+
 pause
